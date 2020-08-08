09c4a641d4714319dc470bfac7d1782e5156faf6
Andrea Vigani <and.vigani@gmail.com>
2020-08-08 12:55:24 +0200

    prices update

--- pages/index.vue
+++ pages/index.vue
@@ -57,9 +57,9 @@
       .container
         h2 Prezzi
         p 
-          span Ecomodel offre diverse di soluzioni per la gestione.
-          span Sono disponibili pacchetti personalizzati annuali per la gestione degli immobili.
-          span Il preventivo viene fatto su misura perché dipende dalla tipologia dell’edificio e dalla tipologia di informazioni che vogliono essere gestite.
+          span Ecomodel offre diverse di soluzioni per la gestione dell’edificio.
+          span Il preventivo per la realizzazione del modello iniziale viene fatto su misura, perché dipende dalla tipologia dell’edificio e dalla quantità di informazioni che in esso vogliono essere gestite.
+          span Una volta realizzato il modello iniziale sono disponibili pacchetti personalizzati per la gestione degli immobili.
         .prices
           .prices__item
             div
@@ -67,25 +67,25 @@
               .prices__item__description Prevede un costo orario per l’aggiornamento del modello, verrà fatto un preventivo prima di iniziare il lavoro.
             div
               .prices__item__price 35€/h
-              a.btn.btn-outline-primary.mb-4(href='#' v-scroll-to="'#contact'") Contattaci
+              a.prices__item__btn.btn.btn-outline-primary.mb-4(href='#' v-scroll-to="'#contact'") Contattaci
           .prices__item
             div
               h4.prices__item__title Pacchetto Base
               .prices__item__description Prevede un aggiornamento del modello mensile di 8 ore.
             div
               .prices__item__price 200€/mese
-              a.btn.btn-outline-primary.mb-4(href='#' v-scroll-to="'#contact'") Contattaci
+              a.prices__item__btn.btn.btn-outline-primary.mb-4(href='#' v-scroll-to="'#contact'") Contattaci
           .prices__item
             div
               h4.prices__item__title Pacchetto Gold
               .prices__item__description Prevede un abbonamento annuale per l’aggiornamento del modello, tutti gli interventi per la gestione del modello sono compresi.
             div
               .prices__item__price 1600€
-              a.btn.btn-outline-primary.mb-4(href='#' v-scroll-to="'#contact'") Contattaci
+              a.prices__item__btn.btn.btn-outline-primary.mb-4(href='#' v-scroll-to="'#contact'") Contattaci
     section.blockquote.bg-primary(v-scroll-reveal)
       .container
         p “Le idee migliori sono proprietà di tutti” 
-        span - Seneca -
+        span.author - Seneca -
     section#contact(v-scroll-reveal)
       .container
         h2 Contattaci o 
@@ -213,8 +213,20 @@ export default {
     transition: all .5s;
     &:hover {
       transform: translateY(-10px);
+      border-color: $secondary;
+      .prices__item__title {
+        background-color: $secondary;
+      }
+      .prices__item__price {
+        color: $secondary;
+      }
+      .prices__item__btn {
+        border-color: $secondary;
+        color: $secondary;
+      }
     }
     &__title {
+      transition: all .5s;
       background-color: $primary;
       padding: 1.5rem 1rem;
       color: $white;
@@ -223,6 +235,7 @@ export default {
       padding: 1.5rem 1rem;
     }
     &__price {
+      transition: all .5s;
       color: $primary;
       font-weight: 700;
       font-size: 2rem;
